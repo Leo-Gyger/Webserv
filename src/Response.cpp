9e56f1fd280043b3f9ab8d79c5634496593c6b2b
Leo Gyger <lgyger@c1r10s3.42lausanne.ch>
2022-03-11 15:08:44 +0100

    Able to deal with the GET request for any file except default

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,5 +1,7 @@
 #include "Response.hpp"
 #include <fstream>
+#include <sstream>
+#include <string>
 Response::Response(void)
 {
 	return;
@@ -11,7 +13,7 @@ std::string	Response::answer(std::string& msg)
 
 	i = msg.find("\r\n");
 	if (i == std::string::npos)
-		std::cout << "AH" << std::endl;
+		return ("HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<!DOCYPE html>\r\n<html><head><title>404</title></head>\r\n<body><center><h1>404 Not Found</h1><br></center></body></html>\r\n");
 	if (!this->is_valid(msg.substr(0, i)))
 		return ("HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<!DOCYPE html>\r\n<html><head><title>404</title></head>\r\n<body><center><h1>404 Not Found</h1><br></center></body></html>\r\n");
 	return (this->request);
@@ -19,27 +21,26 @@ std::string	Response::answer(std::string& msg)
 
 bool	Response::is_valid(std::string demande)
 {
-	std::string::size_type i;
+	std::istringstream s(demande);
 	std::ifstream	file;
 	std::streampos	size;
+	std::string	name;
 
-	i = demande.find("/");
-	if (i != std::string::npos)
-	{
-		std::cout << demande.substr(i) << std::endl;
-		file.open("info.html", std::ios::binary);
-		if (!file.is_open())
-			return false;
-		file.seekg(0, std::ios::end);
-		size = file.tellg();
-		file.seekg(0, std::ios::beg);
-		this->body = std::vector<unsigned char> (size);
-		file.read((char *)&this->body[0], size);
-		this->size_body = size;
-		this->request = "HTTP/1.1 200 OK\r\nContent-Type:text/html; charset=UTF-8\r\n\r\n";
-		return true;
-	}
-	return false;
+	std::getline(s,name, ' ');
+	std::getline(s,name, ' ');
+	name = name.substr(1);
+	std::cout << name << std::endl;
+	file.open(name, std::ios::binary);
+	if (!file.is_open())
+		return false;
+	file.seekg(0, std::ios::end);
+	size = file.tellg();
+	file.seekg(0, std::ios::beg);
+	this->body = std::vector<unsigned char> (size);
+	file.read((char *)&this->body[0], size);
+	this->size_body = size;
+	this->request = "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nContent-Type:text/html; charset=UTF-8\r\n\r\n";
+	return true;
 }
 
 const std::vector<unsigned char>& Response::get_body(void) const
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,9 +1,8 @@
 #include "../includes/Server.hpp"
 #include <fcntl.h>
 #include <vector>
-Server::Server(Socket&	s): s(s)
+Server::Server(Socket& s): s(s)
 {
-	this->s = s;
 	std::cout << "Server constructor" << std::endl;
 	return;
 }
@@ -14,18 +13,21 @@ void	Server::launch(void)
 	Response	r;
 	int	size;
 	std::vector	<unsigned char> body;
-	std::string ans = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<!DOCYPE html>\r\n<html><head><title>Webserv</title></head>\r\n<body><center><h1>Hello World!</h1><br>\r\n<img src=\"test.webp\"</center></body></html>\r\n"; 
-	char buf[30000] = {0};
-	this->fd = accept(s.server_fd, (struct sockaddr *)&s.address, (socklen_t*)&s.addrlen);
-	recv(this->fd, buf, 30000, 0);
-	te = buf;
-	//std::cout << std::endl << te << std::endl;
-	ans = r.answer(te);
-	send(this->fd,ans.c_str(),ans.size(), 0);
-	body = r.get_body();
-	size = r.get_size();
-	send(this->fd,(char *)&body[0], size, 0);
-	close(this->fd);
+	std::string	ans;
+	while (1)
+	{
+		char buf[30000] = {0};
+		this->fd = accept(s.server_fd, (struct sockaddr *)&s.address, (socklen_t*)&s.addrlen);
+		recv(this->fd, buf, 30000, 0);
+		te = buf;
+		std::cout << te << std::endl;
+		ans = r.answer(te);
+		send(this->fd,ans.c_str(),ans.size(), 0);
+		body = r.get_body();
+		size = r.get_size();
+		send(this->fd,(char *)&body[0], size, 0);
+		close(this->fd);
+	}
 }
 
 Server::~Server(void)
