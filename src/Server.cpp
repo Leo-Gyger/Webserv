99bcc22d66b58cd3b21623c19d527b7a3f72ab61
Mano Ségransan <msegrans@student.42lausanne.ch>
2022-04-08 18:20:51 +0200

    Fix full body size

--- src/Server.cpp
+++ src/Server.cpp
@@ -5,24 +5,22 @@
 #include <vector>
 
 Server::Server() : port(200), fd(), bodySize(3000) {}
-void polling(struct pollfd	fds)
+void polling(struct pollfd fds)
 {
-	int	status;
-	do
-	{
+	int status;
+	do {
 		status = poll(&fds, 1, 100);
 	} while (status == 0 && fds.events != fds.revents);
 }
 std::string Server::readSocket() const
 {
 	std::string te;
-	struct pollfd	fds;
+	struct pollfd fds;
 	fds.fd = this->fd;
 	fds.events = POLLIN;
 	polling(fds);
 	char *buf = new char[this->bodySize];
-	for (int in = 0; in != this->bodySize; in++)
-		buf[in] = 0;
+	for (int in = 0; in != this->bodySize; in++) buf[in] = 0;
 	int size = recv(this->fd, buf, this->bodySize, 0);
 	std::cout << size << std::endl;
 	if (size == 0)
@@ -37,75 +35,66 @@ std::string Server::readSocket() const
 	return (te);
 }
 
-void	leave(int sig)
+void leave(int sig)
 {
-	(void)sig;
+	(void) sig;
 	std::exit(1);
 }
 
 void Server::accepting()
 {
-	struct	pollfd fds;
-	int	status;
+	struct pollfd fds;
+	int status;
 
 	fds.events = POLLIN;
 	fds.fd = s->getServerFd();
-	do
-	{
-		status = poll(&fds,1, 10000);
+	do {
+		status = poll(&fds, 1, 10000);
 		std::cout << status << std::endl;
 	} while (status == 0 || fds.events != fds.revents);
-		this->fd = accept(s->getServerFd(), (struct sockaddr *) &s->address,
-					(socklen_t *) &s->addrlen);
+	this->fd = accept(s->getServerFd(), (struct sockaddr *) &s->address,
+					  (socklen_t *) &s->addrlen);
 }
 void Server::launch()
 {
 	std::string te;
 	std::vector<unsigned char> body;
 	std::string ans;
-		if (this->fd == -1)
-		{
-			close(this->fd);
-			return ;
-		}
-		std::string buff = readSocket();
-		std::string serverName =
-			"localhost"; /* TODO: fix hardcoded serverName */
-		Request req(buff, serverName, this->port);
-		while (!req.isFull())
-		{
-			buff = readSocket();
-			if (!req.appendBody(buff))
-			req = Request(buff, serverName, this->port);
-		}
-		Response r(getRoutes(), req);
-		ans = r.getResponse().toString();
-		send(this->fd, ans.c_str(), ans.size(), 0);
-		body = r.getResponse().getBody();
-		send(this->fd, &body[0], body.size(), 0);
+	if (this->fd == -1)
+	{
 		close(this->fd);
+		return;
+	}
+	std::string buff = readSocket();
+	std::string serverName = "localhost"; /* TODO: fix hardcoded serverName */
+	Request req(buff, serverName, this->port);
+	while (!req.isFull())
+	{
+		buff = readSocket();
+		if (!req.appendBody(buff)) req = Request(buff, serverName, this->port);
+	}
+	Response r(getRoutes(), req);
+	ans = r.getResponse().toString();
+	send(this->fd, ans.c_str(), ans.size(), 0);
+	body = r.getResponse().getBody();
+	send(this->fd, &body[0], body.size(), 0);
+	close(this->fd);
 }
 
-Server::~Server() 
+Server::~Server()
 {
-	close (this->fd);
+	close(this->fd);
 	std::cout << "Server destructed" << std::endl;
 }
 
-int	Server::getFd() const
-{
-	return (this->s->getServerFd());
-}
+int Server::getFd() const { return (this->s->getServerFd()); }
 
-void Server::createSocket(const std::string&	addr)
+void Server::createSocket(const std::string &addr)
 {
-	this->s = new Socket(this->port,addr);
+	this->s = new Socket(this->port, addr);
 	this->s->binding();
 }
-void	Server::listen()
-{
-	this->s->listening(10);
-}
+void Server::listen() { this->s->listening(10); }
 
 void Server::setPort(int p) { this->port = p; }
 int Server::getPort() const { return (this->port); }
